e0720e90d73d9d1bf08f166e173b4bbfeacb83d3
Ermal Baki <59308440+ErmalMC@users.noreply.github.com>
2025-07-21 15:41:43 +0200

    Added layout and Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,71 @@
 import picture from './assets/My_Picture.JPG'
 
+const Navbody = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+const Showcase = () => {
+
+    return(
+        <div>
+
+
+        </div>
+    )
+
+}
+
+const About = () =>{
+
+    return(
+        <div className='mx-auto'>
+            <img src={picture}
+                 alt="My_Picture"
+                 className='w-full max-w-sm object-contain'/>
+            <p>
+                
+            </p>
+
+        </div>
+    )
+
+}
+
+
+
+const TotalUI = () => {
+
+    return(
+        <div className="flex justify-center">
+            <div className="w-full max-w-md">
+                <About />
+
+            </div>
+        </div>
+    )
+
+}
+
 
 const App = () => {
 
 
     return (
-        <div>
-            <img src={picture}
-                 alt="My_Picture"
-                 className='w-full max-w-sm object-contain'/>
+        <div className='bg-amber-200 min-h-screen'>
             <h1 className="text-center font-bold text-red-400 text-9xl"> Hello world! </h1>
+            <TotalUI/>
         </div>
 
     )
